350cd4abc9ff11271370384c8d5b1aef07ead5b6
Mahesh <maheshlokhande420@gmail.com>
2024-05-20 21:25:07 +0530

    Aura the sallon webapp deleted

--- Aura - The Saloon/aura-backend/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGO_URI: 'mongodb+srv://nareshlokhande49:iyLI816jFAVkYv9l@aura.tgtbi1e.mongodb.net/aura?retryWrites=true&w=majority&appName=Aura',
-    PORT: 5000
-  };
-  
